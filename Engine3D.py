f3a7bf17a394fb95055e0b2b07473cea3007ced2
Pablo Escobar <77400177+esc20936@users.noreply.github.com>
2022-08-19 17:37:28 -0700

    Add files via upload

--- Engine3D.py
+++ Engine3D.py
@@ -10,7 +10,7 @@ rend = Renderer(width, height)
 # rend.dirLight = V3(-1,0,0)
 
 rend.active_texture = Texture("models/marioT.bmp")
-# rend.background = Texture("models/fondo.bmp")
+rend.background = Texture("models/fondo.bmp")
 rend.glClearBackground()
 # rend.normal_map = Texture("models/model_normal.bmp")
 
@@ -19,11 +19,30 @@ rend.glClearBackground()
 #                  translate = V3(-4, 0, -10),
 #                  scale = V3(4,4,4),
 #                  rotate = V3(0,0,0))
+
 rend.active_shader = gourad
 rend.glLoadModel("models/mario.obj",
                  translate = V3(-9, -2, -10),
                  scale = V3(4,4,4),
                  rotate = V3(0,0,0))
 
-rend.glFinish("pruebas.bmp")
+rend.active_shader = randomBlue
+rend.glLoadModel("models/mario.obj",
+                 translate = V3(-3, -2, -10),
+                 scale = V3(4,4,4),
+                 rotate = V3(0,0,0))
 
+
+rend.active_shader = sinFun
+rend.glLoadModel("models/mario.obj",
+                 translate = V3(3, -2, -10),
+                 scale = V3(4,4,4),
+                 rotate = V3(0,0,0))
+
+rend.active_shader = negative
+rend.glLoadModel("models/mario.obj",
+                 translate = V3(9, -2, -10),
+                 scale = V3(4,4,4),
+                 rotate = V3(0,0,0))
+
+rend.glFinish("salida.bmp")
